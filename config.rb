0df3dfc9feaa981bd79462ad3a59114bb7d5d1e3
Júlio <macos@Julios-iMac.local>
2014-01-08 15:44:07 -0200

    Arrumando a Casa

---
+++ config.rb
@@ -0,0 +1,5 @@
+http_path = "http://localhost/onetoko/"
+css_dir = "lib/style"
+sass_dir = "lib/scss"
+images_dir = "lib/img"
+javascripts_dir = "lib/js"
---
+++ index.php
@@ -0,0 +1,115 @@
+<!doctype html>
+<html class="no-js" lang="pt-BR">
+<head>
+	<meta charset="UTF-8">
+	<title>One Toko</title>
+
+	<!-- [Modernizr] -->
+	<script src="lib/js/modernizr.min.js"></script>
+
+	<!-- [Styles] -->
+	<link rel="stylesheet" href="lib/style/home.css" />
+
+</head>
+<body>
+
+	<!-- [top-al] -->
+		<header class="top-all">
+			
+			<!-- [top-in] -->
+				<div class="top-in">
+					
+					<!-- Logo -->
+					<div class="logo">
+						<img src="lib/img/logo.png" height="34" width="145" alt="Logotipo One Toko" />
+					</div>
+
+					<!-- [search-all] -->
+						<div class="search-all">
+							<form action="#" method="post">
+								
+								<!-- search input -->
+								<input type="text" name="search" value="Pesquisar um Produto"/>
+
+								<!-- Submit -->
+								<button>
+								  <img src="lib/img/search-icon.png" height="16" width="16" alt="Botão Buscar Produto">
+								</button>
+							
+							</form>
+						</div>
+					<!-- [/search-all] -->
+
+					<!-- [user-place] -->
+						<div class="user-place">
+							
+							<!-- user img -->
+
+							<!-- user name -->
+
+							<!-- [user-option] -->
+								<div class="user-option"></div>
+								<div class="user-option_drop"></div>
+							<!-- [/user-option] -->
+
+						</div>
+					<!-- [/user-place] -->
+
+				</div>
+			<!-- [/top-in] -->
+
+		</header>
+	<!-- [/top-al] -->
+
+	<!-- [banner] -->
+		<section class="banner-all">
+			<img src="lib/img/1.png" alt=""/>
+
+			<!-- [banner-content] -->
+				<section class="banner-content">
+					
+					<!-- text -->
+					<div class="banner-text">
+						<h1><span>Motorola Moto X</span></h1>
+						<p>
+							After 25 years, Dr. Dre was tired of spending months on a track 
+							only to have his fans hear it on weak, distorting earbuds. 
+							Two years and hundreds of prototypes later, Beats Studio headphones 
+							are the icons that bring you sound the way it was originally intended.
+						</p>
+						<a href="#">Ver Mais</a>
+					</div>
+					
+					<!-- switch -->
+					<div class="switch">
+						<ul>
+							<li class="active"></li>
+							<li></li>
+							<li></li>
+							<li></li>
+						</ul>
+					</div>
+
+				</section>
+			<!-- [/banner-content] -->
+
+		</section>
+	<!-- [/banner] -->
+
+	<!-- [content] -->
+		<main class="content">
+			
+			<!-- [slider] -->
+				<section class="slider-all">
+					<div class="setas prev"></div>
+					
+
+
+				</section>
+			<!-- [/slider] -->
+
+		</main>
+	<!-- [/content] -->
+	
+</body>
+</html>
