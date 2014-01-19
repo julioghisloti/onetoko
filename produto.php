82efb4715eccc72d4779fc1999d91012b9f5ff57
Julio <juliohisloti@gmail.com>
2014-01-19 21:02:12 -0200

    Continuando a pagina de produto

--- produto.php
+++ produto.php
@@ -6,16 +6,16 @@
 	<title>Nome do Produto - One Toko</title>
 
 	<!-- [Modernizr] -->
-	<script src="lib/js/modernizr.min.js"></script>
+	<script src="build/js/modernizr.min.js"></script>
 
 	<!-- [Styles] -->
-	<link rel="stylesheet" href="lib/style/produto.css" />
+	<link rel="stylesheet" href="build/style/produto.css" />
 
 </head>
 <body>
 
 	<!-- [top] -->
-	<?php include "lib/comp/topo.php"; ?>
+	<?php include "src/comp/topo.php"; ?>
 
 	<!-- [content] -->
 		<main class="content">
@@ -27,15 +27,19 @@
 
 					<!-- Fotos -->
 					<div class="prod-img">
-						<div class="prev"></div>
-						<div class="next"></div>
+						<div class="setas prev"></div>
+						<div class="setas next"></div>
 
-						<ul>
-							<li><img src="lib/img/moto-x/motox_azul-02.jpg" alt=""></li>
-							<li><img src="lib/img/moto-x/motox_azul-04.jpg" alt=""></li>
-							<li><img src="lib/img/moto-x/motox_azul-05.jpg" alt=""></li>
-							<li><img src="lib/img/moto-x/motox_azul-08.jpg" alt=""></li>
-						</ul>
+						<div class="prod-img-content">
+							<ul>
+								<li><img src="src/img/moto-x/motox_azul-02.jpg" alt=""></li>
+								<li><img src="src/img/moto-x/motox_azul-04.jpg" alt=""></li>
+								<li><img src="src/img/moto-x/motox_azul-05.jpg" alt=""></li>
+								<li><img src="src/img/moto-x/motox_azul-08.jpg" alt=""></li>
+								<li></li>
+								<li></li>
+							</ul>
+						</div>
 					</div>
 					
 					<!-- [opniao leitores] -->
@@ -65,8 +69,215 @@
 				</aside>
 			<!-- [/produto] -->
 
+			<!-- [produto info] -->
+				<section class="prod-info col _70pc">
+
+				<!-- [reviews] -->
+					<section>
+						
+						<!-- Section Title -->
+						<h1 class="title"><span>Reviews</span></h1>
+
+						<!-- content - repetir esse bloco -->
+						<section class="content-info">
+							<h1>Moto X, o smartphone da Motorola com o toque do Google</h1>
+							<p>
+								O primeiro celular da Motorola desenvolvido depois que a companhia foi comprada 
+								pelo Google traz o melhor da tecnologia atual, tudo combinado para criar uma 
+								experiência completa em termos de recursos e serviços disponíveis por padrão no 
+								aparelho...
+								<a href="#">Leia mais</a>
+							</p>
+
+							<strong><a href="#">via tecnoblog</a></strong></li>	
+						</section>
+
+						<!-- content - repetir esse bloco -->
+						<section class="content-info">
+							<h1>O Moto X é um excelente representante da Motorola após a compra pelo Google</h1>
+							<p>
+								O Moto X é a primeira grande novidade da Motorola na era Google. Com design 
+								diferenciado e recursos exclusivos, o smartphone promete fazer jus ao título de 
+								“celular inteligente”...
+								<a href="#">Leia mais</a>
+							</p>
+
+							<strong><a href="#">via techtudo</a></strong></li>	
+						</section>
+
+					</section>
+				<!-- [/reviews] -->
+
+				<!-- [especificacoes] -->
+					<section class="especificacoes">
+
+						<!-- Section Title -->
+						<h1 class="title"><span>Especificações</span></h1>
+
+						<section class="content-info col _50pc">
+							 <p>
+							 	<strong>Câmera Frontal</strong><br/>
+							 	2MP, 1080p HD video
+							 </p>
+
+							  <p>
+							 	<strong>Câmera Traseira</strong><br/>
+								10MP, 1080p HD video (30fps)
+							 </p>
+
+							 <p>
+							 	<strong>Visor</strong> <br/>
+								4.7” AMOLED (RGB) / HD 720p, Corning® 
+								Gorilla™ Glass, Active display technology 
+							 </p>
+
+							 <p>
+							 	<strong>Wi-Fi</strong> <br/>
+								802.11a/g/b/n/ac (habilitado para banda 
+								dupla), hotspot móvel
+							 </p>
+
+							 <p>
+							 	<strong>GPS e serviços de localização</strong> <br/>
+								aGPS (assistido), AGPS (autônomo), sGPS 
+								(simultâneo), GPS Standalone, eCompass, 
+								GLONASS
+							 </p>
+
+							 <p>
+							 	<strong>Sistema operacional</strong> <br/>
+								Android 4.2.2
+							 </p>
+						</section>
+
+						<section class="content-info col _50pc">
+							 <p>
+							 	<strong>Câmera Frontal</strong><br/>
+							 	2MP, 1080p HD video
+							 </p>
+
+							  <p>
+							 	<strong>Câmera Traseira</strong><br/>
+								10MP, 1080p HD video (30fps)
+							 </p>
+
+							 <p>
+							 	<strong>Visor</strong> <br/>
+								4.7” AMOLED (RGB) / HD 720p, Corning® 
+								Gorilla™ Glass, Active display technology 
+							 </p>
+
+							 <p>
+							 	<strong>Wi-Fi</strong> <br/>
+								802.11a/g/b/n/ac (habilitado para banda 
+								dupla), hotspot móvel
+							 </p>
+
+							 <p>
+							 	<strong>GPS e serviços de localização</strong> <br/>
+								aGPS (assistido), AGPS (autônomo), sGPS 
+								(simultâneo), GPS Standalone, eCompass, 
+								GLONASS
+							 </p>
+
+							 <p>
+							 	<strong>Sistema operacional</strong> <br/>
+								Android 4.2.2
+							 </p>
+						</section>
+
+					</section>
+				<!-- [/especificacoes] -->
+
+				<!-- [duvidas] -->
+					<section class="duvidas">
+						
+						<!-- Section Title -->
+						<h1 class="title">
+							<div class="section-title" id="duvida-label">
+								Dúvidas <div class="seta"></div>
+							</div>
+
+							<div class="section-title disabled" id="reclame-label">
+								Reclamações <div class="seta"></div>
+							</div>
+
+							<div class="section-title disabled" id="reclame-label">
+								Comentários <div class="seta"></div>
+							</div>
+						</h1>
+
+						<!-- [duvidas - content] -->
+							<section class="duvidas-content">
+								<!-- [submit duvida] -->
+									<div class="block">
+										<h1>Alguma duvida?</h1>
+										<div class="content-block">
+											<input type="text" name="duvida"/>
+											<input type="submit" class="bt" value="Perguntar" />
+										</div>
+									</div>
+								<!-- [/submit duvida] -->
+
+								<!-- content - repetir esse bloco -->
+								<section class="content-info">
+									<h1>O Moto X e o Nexus 4 possuem gravador?</h1>
+									<p>
+										Nexus eu sei que não tem, mas possui um microfone que, com o app correto, 
+										deve gravar bem o áudio. Já sobre o Moto X eu não posso confirmar nada mas 
+										creio que também não... 
+										<a href="#">7 respostas</a>
+									</p>
+									<div class="content-info-options">
+										<ul>
+											<li>sobre o <a href="#">Moto X</a></li>
+											<li>
+												<i class="icon responder"></i> 
+												<strong><a href="#">responder</a></strong>
+											</li>
+										</ul>
+									</div>
+								</section>
+								<!-- content -->
+
+								<!-- content - repetir esse bloco -->
+								<section class="content-info">
+									<h1>Duvidas com o Moto X</h1>
+									<p>
+										Fui usuário da familia Nexus desde os aparelhos HTC e isso me fez pegar o 
+										costume de usar android puro sempre em sua última versão. Infelizmente meu 
+										Nexus 4 caiu e se espatifou, me obrigando ...  
+										<a href="#">3 respostas</a>
+									</p>
+									<div class="content-info-options">
+										<ul>
+											<li>sobre o <a href="#">Moto X</a></li>
+											<li>
+												<i class="icon responder"></i> 
+												<strong><a href="#">responder</a></strong>
+											</li>
+										</ul>
+									</div>
+								</section>
+								<!-- content -->
+
+							</section>
+						<!-- [/duvidas - content] -->
+
+					</section>
+				<!-- [/duvidas] -->
+
+				</section>
+			<!-- [/produto info] -->
+
 		</main>
 	<!-- [/content] -->
+
+	<!-- [script] -->
+	<script src="build/js/jquery.min.js"></script>
+	<script src="build/js/jcarousellite.min.js"></script>
+	<script src="build/js/produto.js"></script>
+
 	
 </body>
 </html>
